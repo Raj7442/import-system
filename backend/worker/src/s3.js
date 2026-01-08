752249cc9c94ef940d03128af5ad2d9d43a04bb7
Raj <162421365+Raj7442@users.noreply.github.com>
2026-01-08 13:53:02 +0530

    Update s3.js

--- backend/worker/src/s3.js
+++ backend/worker/src/s3.js
@@ -1,1 +1,15 @@
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3({
+  region: process.env.AWS_REGION,
+});
+
+export async function uploadToS3(buffer, key, mime) {
+  return s3.upload({
+    Bucket: process.env.S3_BUCKET,
+    Key: key,
+    Body: buffer,
+    ContentType: mime,
+  }).promise();
+}
 
