f2a9db6f9c5c0f8557b0e88d3119ef5384484a78
Raj <162421365+Raj7442@users.noreply.github.com>
2026-01-08 13:51:42 +0530

    Add ImportForm and ImageList components to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,13 @@
+import ImportForm from "./ImportForm";
+import ImageList from "./ImageList";
+
+export default function App() {
+  return (
+    <>
+      <h1>Image Importer</h1>
+      <ImportForm />
+      <ImageList />
+    </>
+  );
+}
 
