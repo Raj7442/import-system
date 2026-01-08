9ca8ad3076ea8b9b61e12ce04fd38b901ddc9263
Raj <162421365+Raj7442@users.noreply.github.com>
2026-01-08 13:51:59 +0530

    Add API functions for importing and fetching images

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,12 @@
+const API = "http://localhost:3000";
+
+export const importImages = (folderUrl) =>
+  fetch(`${API}/import/google-drive`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ folderUrl }),
+  });
+
+export const fetchImages = () =>
+  fetch(`${API}/images`).then(res => res.json());
 
