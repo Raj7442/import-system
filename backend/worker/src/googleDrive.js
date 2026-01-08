6876397c22a23d0c0bf25839afaa3fc2da49fa33
Raj <162421365+Raj7442@users.noreply.github.com>
2026-01-08 13:53:22 +0530

    Update googleDrive.js

--- backend/worker/src/googleDrive.js
+++ backend/worker/src/googleDrive.js
@@ -1,1 +1,8 @@
+import axios from "axios";
+
+export async function fetchImages(folderId) {
+  const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&key=${process.env.GDRIVE_API_KEY}`;
+  const res = await axios.get(url);
+  return res.data.files;
+}
 
